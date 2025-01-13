ee378820d576a01d639ea459132551b3f110a534
Yuyuan1110 <z0975750027@gmail.com>
2025-01-13 17:53:06 +0800

    update treesitter, try to setup function area folding, not still have a few issue.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -2,22 +2,39 @@ return{
 	{
 		"nvim-treesitter/nvim-treesitter",
 		config = function()
-			require("lazy").setup({
-				{
-					"nvim-treesitter/nvim-treesitter", 
-					build = ":TSUpdate"
-				}
-			})
+--			require('lazy').setup({
 			require('nvim-treesitter.configs').setup({
 				ensure_installed = {
+					"bash",
+					"ssh_config",
+					"diff",
+					"java",
+					"kotlin",
+					"html",
+					"css",
+					"javascript",
+					"php",
+					"nginx",
 					"c", 
+					"cpp",
+					"make",
+					"python",
+					"perl",
 					"lua", 
 					"vim", 
 					"vimdoc", 
 					"query", 
 					"markdown", 
-					"markdown_inline"
+					"markdown_inline",
+					"sql",
 				},
+				build = ":TSUpdate",
+				indent = { enable = true },
+				fold = {
+					enable = true, 
+					fold_level_start = 0,  
+					fold_level = 99,  
+				}
 			})
 		end
 	},
