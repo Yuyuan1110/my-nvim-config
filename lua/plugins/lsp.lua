1c8559f62d34955c98247f953b16ef80df108617
Wang.Yuan <wang.yuan@inventec.com>
2026-02-02 17:19:46 +0800

    Update: Split lsp and cpmletion feature
    Extract the completion feature from lua.lsp into completion.lsp, make
    the lsp.lua more singular.
    more specify.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,7 +42,7 @@ return {
 					},
 					dotls = {},
 				},
-				require('lazydev').setup()
+			require('lazydev').setup()
 			require('mason').setup()
 
 			-- key mapping settings have been moved to config/keymaps.lua
@@ -112,24 +112,6 @@ return {
 						capabilities = capabilities,
 					})
 				},
-			})
-			local cmp = require('cmp')
-			cmp.setup({
-				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'path' },
-					{ name = 'buffer' },
-				}),
-				window = {
-					completion = cmp.config.window.bordered(),
-				}
 			})
 		end,
 	},
